e7531f0e1a1f198314e80cf1afe46902c238306f
icedlee337 <lee.e.norris@gmail.com>
2024-12-30 12:48:26 -0600

    chore: make access key and secret optional

--- src/lib/classes/server-aws-s3-config.class.ts
+++ src/lib/classes/server-aws-s3-config.class.ts
@@ -1,3 +1,7 @@
 export class ServerAwsS3Config {
-    constructor(public AWS_ACCESS_KEY_ID: string, public AWS_REGION: string, public AWS_SECRET_ACCESS_KEY: string, public AWS_BUCKET: string) {}
+    AWS_ACCESS_KEY_ID?: string;
+    AWS_REGION: string;
+    AWS_SECRET_ACCESS_KEY?: string;
+    AWS_BUCKET: string;
+    NODE_ENV: string;
 }
--- src/lib/server-aws-s3.module.ts
+++ src/lib/server-aws-s3.module.ts
@@ -4,8 +4,6 @@ import { S3Service } from './services/s3.service';
 import { moduleFactory } from '@onivoro/server-common';
 import { ServerAwsS3Config } from './classes/server-aws-s3-config.class';
 
-// todo: add auth resolver to use iam role for deployment but creds locally
-
 @Module({})
 export class ServerAwsS3Module {
   static configure(config: ServerAwsS3Config) {
@@ -16,7 +14,15 @@ export class ServerAwsS3Module {
           provide: S3Service,
           useFactory: () => new S3Service(
             config,
-            new S3Client({ region: config.AWS_REGION })
+            new S3Client({
+              region: config.AWS_REGION,
+              credentials: config.NODE_ENV === 'production'
+                ? undefined
+                : {
+                  accessKeyId: config.AWS_ACCESS_KEY_ID,
+                  secretAccessKey: config.AWS_SECRET_ACCESS_KEY
+                }
+            })
           )
         }
       ]
